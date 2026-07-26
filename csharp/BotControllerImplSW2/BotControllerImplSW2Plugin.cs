4307c2fcb1f16d0b1619ebe59d52bef17c4e17f0
yuanyuan829 <y17611630829@yeah.net>
2026-07-26 23:41:31 +0800

    docs: Updated version number

--- csharp/BotControllerImplSW2/BotControllerImplSW2Plugin.cs
+++ csharp/BotControllerImplSW2/BotControllerImplSW2Plugin.cs
@@ -19,7 +19,7 @@ namespace BotControllerImplSW2;
 
 [PluginMetadata(
     Id = "botcontroller.sw2",
-    Version = "0.5.3",
+    Version = "0.5.4",
     Name = "BotController",
     Author = "XBribo & nicedayzhu",
     Description = "Record a player's movement and replay it on a bot."
